c766a973dde0cbd3261c8a9640bb44e4cd4cfc0d
Asad <asadabbassherazi@gmail.com>
2025-06-06 22:07:46 +0500

    Configure URL routing for ETL app

---
+++ etl/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'etl'
+
+urlpatterns = [
+    path('', views.etl_dashboard, name='etl_dashboard'),
+    path('status/', views.etl_status, name='status'),
+    path("run/", views.ETLRunView.as_view(), name="run-etl"),
+]
